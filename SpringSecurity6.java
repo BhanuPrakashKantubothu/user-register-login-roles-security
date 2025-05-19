b86cca6efe727227880b889a2a6f6e6555a61196
BhanuPrakashKantubothu <136150038+BhanuPrakashKantubothu@users.noreply.github.com>
2025-05-19 18:06:39 +0530

    Add files via upload

---
+++ SpringSecurity6.java
@@ -0,0 +1,13 @@
+package com.example.springSecurity6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity6 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity6.class, args);
+	}
+
+}
